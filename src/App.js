971064a0e7e32d67f406eb0baf709721afe2128a
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-03-19 11:09:56 -0600

    Added state and props to parent component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import { Navbar } from "./components/Navbar";
 import { Main } from "./components/Main";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  function toggleDarkMode() {
+    setDarkMode((prevMode) => !prevMode);
+  }
+
   return (
-    <div className="App">
-      <Navbar />
-      <Main />
+    <div className="container">
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Main darkMode={darkMode} />
     </div>
   );
 }
