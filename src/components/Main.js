49fe18c18c9a5677cfcc2b3e01deeeffffaa8956
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-03-19 15:24:42 -0600

    Added new components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,14 @@
-export function Main() {
+export function Main(props) {
   return (
-    <>
-      <h1></h1>
-    </>
+    <main className={props.darkMode ? "dark" : ""}>
+      <h1 className="main--title">Fun facts about the USA</h1>
+      <ul className="main--facts">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </main>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,17 @@
-export function Navbar() {}
+export function Navbar(props) {
+  const { darkMode, toggleDarkMode } = props;
+
+  return (
+    <nav className={darkMode ? "dark" : ""}>
+      <h3 className="nav--logo-text">USAFacts</h3>
+
+      <div className="toggler">
+        <p className="toggler--light">Light</p>
+        <div className="toggler--slider" onClick={toggleDarkMode}>
+          <div className="toggler--slider--circle"></div>
+        </div>
+        <p class="toggler--dark">Dark</p>
+      </div>
+    </nav>
+  );
+}
