5659d91b92554e79770ee65286a0b4db51a8074b
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-03-20 08:59:29 -0600

    Added fun facts data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,24 @@
+import { useState } from "react";
+import data from "../data";
+
 export function Main(props) {
+  const [count, setCount] = useState(10);
+  const items = data
+    .slice(0, count)
+    .map((item) => <li key={item.id}>{item.description}</li>);
+
+  function handleClick() {
+    setCount(count + 10);
+  }
+
+  // show the button ONLY when count is less than the length of the data array
+  const showBtn = count < data.length;
+
   return (
     <main className={props.darkMode ? "dark" : ""}>
       <h1 className="main--title">Fun facts about the USA</h1>
-      <ul className="main--facts">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+      <ol className="main--facts">{items}</ol>
+      {showBtn && <button onClick={handleClick}>Load More</button>}
     </main>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,154 @@
+export default [
+  {
+    id: 1,
+    description:
+      "The United States is the third largest country in the world by landmass – nearly as big as the entire continent of Europe.",
+  },
+  {
+    id: 2,
+    description:
+      "American athletes have won more Olympic medals than athletes from any other country.",
+  },
+  {
+    id: 3,
+    description:
+      "The United States is the only country that has all of Earth's five climate zones: tropical, dry, temperate, continental, and polar.",
+  },
+  {
+    id: 4,
+    description:
+      "The United States is the third most visited country by international tourists after Spain and France.",
+  },
+  {
+    id: 5,
+    description:
+      "Lake Superior, on the United States-Canada border, is the largest body of freshwater in the world by surface area. The lake covers 31,700 square miles.",
+  },
+  {
+    id: 6,
+    description:
+      "The Pentagon – the headquarters of the Department of Defense – is the world's largest office building by floor area.",
+  },
+  {
+    id: 7,
+    description:
+      "The only people to have walked on the moon's surface were Americans.",
+  },
+  {
+    id: 8,
+    description:
+      "The United States is home to nearly 45 million immigrants – more than any other country in the world.",
+  },
+  {
+    id: 9,
+    description:
+      "Much of the music the world listens to – jazz, rock 'n' roll, hip-hop, country, folk, R&B, soul, and gospel – comes out of the United States.",
+  },
+  {
+    id: 10,
+    description:
+      "The U.S. film industry is the largest, oldest, and most profitable film industry in the world. According to research firm IBISWorld, the American film industry generated about $33 billion in 2019.",
+  },
+  {
+    id: 11,
+    description:
+      "More than nine in every 10 Americans have eaten pizza in the last month. According to some estimates, Americans consume the equivalent of about 100 acres of pizza every day.",
+  },
+  {
+    id: 12,
+    description:
+      "Nearly one-third of all land in the United States – or about 650 million acres – is federally owned.",
+  },
+  {
+    id: 13,
+    description:
+      "On any given day in the United States, about 84.8 million adults – 37% of the adult population – consume fast food, according to the Centers for Disease Control and Prevention.",
+  },
+  {
+    id: 14,
+    description:
+      "According to the USDA, foreign investors own at least 28.3 million acres of U.S. farmland, an area about the size of Ohio.",
+  },
+  {
+    id: 15,
+    description:
+      "The United States was the first nation to use the title of President for its head of state.",
+  },
+  {
+    id: 16,
+    description:
+      "The United States is at the center of global invention. Airplanes, computers, cellphones, potato chips, and the light bulb are just a few examples of American ingenuity.",
+  },
+  {
+    id: 17,
+    description:
+      "There are an estimated 75.8 million dogs in the United States, more than double the number in Brazil, the country with the second most dogs.",
+  },
+  {
+    id: 18,
+    description:
+      "Though the United States is home to less than 5% of the world's population, it accounts for approximately 25% of global economic output.",
+  },
+  {
+    id: 19,
+    description:
+      "The United States became the world's largest petroleum producer in the last 10 years. The country produced 18.23 million barrels of oil per day in 2019, well above the daily production of Saudi Arabia, the world's second-biggest oil producer. Oil includes crude oil, all other petroleum liquids, and biofuels.",
+  },
+  {
+    id: 20,
+    description:
+      "The United States is also the world's largest consumer of oil. Gasoline, distillate fuel oil, hydrocarbon gas liquids, and jet fuel consumption combine for a total of 20.48 million barrels of oil consumed daily.",
+  },
+  {
+    id: 21,
+    description:
+      "The current iteration of the American flag was designed by a high school student.",
+  },
+  {
+    id: 22,
+    description:
+      "There are more guns than people in the United States – about 101 for every 100 people, according to some estimates. The country with the next highest ownership rate is Serbia, where there are 58 guns for every 100 people.",
+  },
+  {
+    id: 23,
+    description:
+      "Approximately 43 million Americans identify as ancestrally German, more than any other nationality.",
+  },
+  {
+    id: 24,
+    description:
+      "The United States is one of only three countries that have not officially switched to the metric system. Liberia and Burma are the other two.",
+  },
+  {
+    id: 25,
+    description:
+      "The typical wage or salary worker in the United States has been with his or her current employer for 4.2 years.",
+  },
+  {
+    id: 26,
+    description:
+      "The government chose to color U.S. currency green as an anti-counterfeiting strategy. When the color was adopted in the 19th century, cameras could only take black and white photographs, making imitation bills difficult to produce.",
+  },
+  {
+    id: 27,
+    description:
+      "Over half of the U.S. population lives in just nine states: California, Texas, Florida, New York, Pennsylvania, Illinois, Ohio, Georgia, and North Carolina.",
+  },
+  {
+    id: 28,
+    description:
+      "The number of passports Americans held leaped to 21.4 million in 2017, the most ever recorded.",
+  },
+  {
+    id: 29,
+    description:
+      "According to a recent Pew Research Center study, 57% of Americans believe that individuals control their own success in life, the largest share of any country surveyed.",
+  },
+  {
+    id: 30,
+    description:
+      "The United States is one of most mobile nations on Earth. According to a Gallup poll, 24% of U.S. adults reported moving within the country in the past five years.",
+  },
+];
+
+// Credits: https://www.usatoday.com/story/money/2020/03/06/56-most-amazing-things-about-america-today/41212433/
